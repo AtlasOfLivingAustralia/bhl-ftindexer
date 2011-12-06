64f843ae050098ed7ee3cb965c2730649524e897
davidbairdala <david.baird.ala@gmail.com>
2011-12-06 22:45:16 +0000

    Fixed test folders
    git-svn-id: http://bhl-au-ftindex.googlecode.com/svn/trunk/ftindexer@3 4c5483bf-a69c-9e22-618e-186b669915a2

---
+++ src/test/java/au/org/ala/bhl/TaxonGrabTest.java
@@ -0,0 +1,41 @@
+package au.org.ala.bhl;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+public class TaxonGrabTest {
+    
+    @Test
+    public void test1() {
+        TaxonGrab tg = new TaxonGrab();
+        tg.findNames("bcd asdhj jHAS Kjash");
+    }
+    
+    @Test
+    public void testNormalize1() {
+        TaxonGrab tg = new TaxonGrab();
+        String actual = tg.normalizeText("x -\rx -\nx:x;x.", TaxonGrab.SYMBOLS);
+        
+        System.err.println(actual);
+    }
+    
+    @Test
+    public void testFind1() throws Exception {
+        
+        InputStream is = TaxonGrabTest.class.getResourceAsStream("/sample1.txt");
+        String data = IOUtils.toString(is);
+        
+        TaxonGrab tg = new TaxonGrab();
+        List<String> names = tg.findNames(data);
+        
+        for (String name : names) {
+            System.err.println(name);
+        }
+        
+    }
+
+
+}
